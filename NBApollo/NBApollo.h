7e06e1db13f253dd818275ae0047665c18e1beb7
Josman Pérez Expósito <josman.perez@udc.es>
2020-03-31 10:11:40 +0200

    Initial Commit

---
+++ NBApollo/NBApollo.h
@@ -0,0 +1,19 @@
+//
+//  NBApollo.h
+//  NBApollo
+//
+//  Created by Josman Pérez Expósito on 31/03/2020.
+//  Copyright © 2020 Josman Pérez Expósito. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NBApollo.
+FOUNDATION_EXPORT double NBApolloVersionNumber;
+
+//! Project version string for NBApollo.
+FOUNDATION_EXPORT const unsigned char NBApolloVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NBApollo/PublicHeader.h>
+
+
